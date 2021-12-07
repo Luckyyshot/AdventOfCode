478eff60c2dea0c77632be5b7230e155e49799aa
Luckyyshot <22941690+Luckyyshot@users.noreply.github.com>
2021-12-07 07:12:28 +0100

    7th day solution.

---
+++ 2021/7/TreacheryWhales/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TreacheryWhales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = System.IO.File.ReadAllText(@"D:\Repositories\AdventOfCode\2021\7\input.txt").Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            //Part 1
+            int[] result = Enumerable.Repeat(0, input.Max() - input.Min()).ToArray();
+            foreach (var crab in input)
+            {
+                for (int i = 0; i < result.Length; i++)
+                {
+                    result[i] += Math.Abs(crab - i);
+                }
+            }
+
+            Console.WriteLine(result.Min());
+
+            //Part 2
+            int[] result2 = Enumerable.Repeat(0, input.Max() - input.Min()).ToArray();
+            foreach (var crab in input)
+            {
+                for (int i = 0; i < result2.Length; i++)
+                {
+                    var n = Math.Abs(crab - i);
+                    result2[i] += n * (n + 1) / 2;
+                }
+            }
+
+            Console.WriteLine(result2.Min());
+        }
+    }
+}
