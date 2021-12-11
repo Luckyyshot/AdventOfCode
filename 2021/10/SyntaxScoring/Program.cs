b905bd96020b796da917489dd2b510aa1451e97c
Luckyyshot <22941690+Luckyyshot@users.noreply.github.com>
2021-12-11 19:07:26 +0100

    10th day solution.

---
+++ 2021/10/SyntaxScoring/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SyntaxScoring
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = System.IO.File.ReadAllText(@"D:\Repositories\AdventOfCode\2021\10\input.txt").Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+
+            //Part 1
+            int totalScore = 0;
+            foreach (var str in input)
+            {
+                totalScore += SyntaxReader.CalcSyntaxErrorScore(str);
+            }
+
+            Console.WriteLine("Total syntax error score: " + totalScore);
+
+            //Part 2
+            List<double> scores = new();
+            foreach (var str in input)
+            {
+                scores.Add(SyntaxReader.CalcSyntaxCompletionScore(str));
+            }
+            scores = scores.Where(i => i != 0).ToList();
+            scores.Sort();
+
+            Console.WriteLine("Middle score of the completion string scores: " + scores[scores.Count / 2]);
+        }
+    }
+}
---
+++ 2021/10/SyntaxScoring/SyntaxReader.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyntaxScoring
+{
+    class SyntaxReader
+    {
+        public static int CalcSyntaxErrorScore(string line)
+        {
+            Stack<char> syntaxCheck = new ();
+
+            foreach (var chr in line)
+            {
+                if (chr == '(' || chr == '[' || chr == '{' || chr == '<')
+                    syntaxCheck.Push(chr);
+                if (chr == ')' || chr == ']' || chr == '}' || chr == '>')
+                {
+                    if (chr == ')' && syntaxCheck.Peek() != chr && syntaxCheck.Peek() != '(')
+                    {
+                        syntaxCheck.Pop();
+                        return 3;
+                    }
+                    if (chr == ']' && syntaxCheck.Peek() != chr && syntaxCheck.Peek() != '[')
+                    {
+                        syntaxCheck.Pop();
+                        return 57;
+                    }
+                    if (chr == '}' && syntaxCheck.Peek() != chr && syntaxCheck.Peek() != '{')
+                    {
+                         syntaxCheck.Pop();
+                        return 1197;
+                    }
+                    if (chr == '>' && syntaxCheck.Peek() != chr && syntaxCheck.Peek() != '<')
+                    {
+                        syntaxCheck.Pop();
+                        return 25137;
+                    }
+                    syntaxCheck.Pop();
+                }
+            }
+            return 0;
+        }
+
+        public static double CalcSyntaxCompletionScore(string line)
+        {
+            Stack<char> syntaxCheck = new ();
+            double score = 0;
+
+            foreach (var chr in line)
+            {
+                if (chr == '(' || chr == '[' || chr == '{' || chr == '<')
+                    syntaxCheck.Push(chr);
+                if (chr == ')' || chr == ']' || chr == '}' || chr == '>')
+                {
+                    char nxtChr = syntaxCheck.Pop();
+                    if (chr == ')' && nxtChr != chr && nxtChr != '(')
+                        return 0;
+                    if (chr == ']' && nxtChr != chr && nxtChr != '[')
+                        return 0;
+                    if (chr == '}' && nxtChr != chr && nxtChr != '{')
+                        return 0;
+                    if (chr == '>' && nxtChr != chr && nxtChr != '<')
+                        return 0;
+                    if (chr == ')' && nxtChr == '(')
+                        continue;
+                    if (chr == ']' && nxtChr == '[')
+                        continue;
+                    if (chr == '}' && nxtChr == '{')
+                        continue;
+                    if (chr == '>' && nxtChr == '<')
+                        continue;
+                }
+            }
+
+            string completionStr = "";
+            int count = syntaxCheck.Count;
+            for (int i = 0; i < count; i++)
+            {
+                char nxtChr = syntaxCheck.Pop();
+                if (nxtChr == '(')
+                    score = score * 5 + 1;
+                if (nxtChr == '[')
+                    score = score * 5 + 2;
+                if (nxtChr == '{')
+                    score = score * 5 + 3;
+                if (nxtChr == '<')
+                    score = score * 5 + 4;
+                completionStr += nxtChr;
+            }
+            return score;
+        }
+    }
+}
