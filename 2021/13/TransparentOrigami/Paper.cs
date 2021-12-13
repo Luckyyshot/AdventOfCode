490e886208b1446c4eb096f4839159ecab011882
Luckyyshot <22941690+Luckyyshot@users.noreply.github.com>
2021-12-13 19:22:34 +0100

    13th day solution.

---
+++ 2021/13/TransparentOrigami/Paper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransparentOrigami
+{
+    class Paper
+    {
+        readonly char[,] paper;
+
+        public Paper(string[] input, int x, int y)
+        {
+            paper = new char[x+1, y+1];
+            for (int i = 0; i <= x; i++)
+                for (int j = 0; j <= y; j++)
+                    paper[i, j] = '.';
+
+            foreach (var coord in input)
+            {
+                string[] temp = coord.Split(",");
+                paper[int.Parse(temp[0]), int.Parse(temp[1])] = '#';
+            }
+        }
+
+        public Paper(char[,] input)
+        {
+            paper = input;
+        }
+
+        public int CountDots()
+        {
+            int result = 0;
+            for (int i = 0; i < paper.GetLength(0); i++)
+                for (int j = 0; j < paper.GetLength(1); j++)
+                    if (paper[i, j] == '#')
+                        result++;
+            return result;
+        }
+
+        public char[,] Fold(bool x)
+        {
+            char[,] result;
+            if (x)
+            {
+                result = new char[(paper.GetLength(0) - 1) / 2, paper.GetLength(1)];
+
+                for (int i = 0; i < result.GetLength(0); i++)
+                {
+                    for (int j = 0; j < result.GetLength(1); j++)
+                    {
+                        result[i, j] = paper[i, j];
+                        if (paper[paper.GetLength(0) - i - 1, j] == '#')
+                            result[i, j] = '#';
+                    }
+                }
+            }
+            else
+            {
+                result = new char[paper.GetLength(0), (paper.GetLength(1) - 1) / 2];
+
+                for (int i = 0; i < result.GetLength(0); i++)
+                {
+                    for (int j = 0; j < result.GetLength(1); j++)
+                    {
+                        result[i, j] = paper[i, j];
+                        if (paper[i, paper.GetLength(1) - j - 1] == '#')
+                            result[i, j] = '#';
+                    }
+                }
+            }
+
+            return result;
+        }
+
+
+        public override string ToString()
+        {
+            string result = "";
+            for (int i = 0; i < paper.GetLength(1); i++)
+            {
+                for (int j = 0; j < paper.GetLength(0); j++)
+                {
+                    result += paper[j, i];
+                }
+                result += "\r\n";
+            }
+            return result;
+        }
+    }
+}
---
+++ 2021/13/TransparentOrigami/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TransparentOrigami
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = System.IO.File.ReadAllText(@"D:\Repositories\AdventOfCode\2021\13\input.txt").Split("\r\n\r\n", StringSplitOptions.RemoveEmptyEntries);
+            string[] foldInstructions = input[1].Split("\r\n");
+            input = input[0].Split("\r\n");
+
+            int x = 0, y = 0;
+            if (foldInstructions[0][11] == 'x')
+            {
+                x = int.Parse(foldInstructions[0][13..]) * 2;
+                y = int.Parse(foldInstructions[1][13..]) * 2;
+            }
+            else
+            {
+                x = int.Parse(foldInstructions[1][13..]) * 2;
+                y = int.Parse(foldInstructions[0][13..]) * 2;
+            }
+
+            //Part 1
+            Paper paper = new(input, x, y);
+
+            paper = new Paper(paper.Fold(foldInstructions[0][11] == 'x'));
+
+            //Console.WriteLine(test.ToString());  //Run at own risk.
+            Console.WriteLine("Empty spaces: " + paper.CountDots());
+
+            //Part 2
+            paper = new(input, x, y);
+            foreach (var instruction in foldInstructions)
+            {
+                if (instruction[11] == 'x')
+                    paper = new Paper(paper.Fold(true));
+                else
+                    paper = new Paper(paper.Fold(false));
+            }
+
+            Console.WriteLine(paper.ToString());
+        }
+    }
+}
